4f97cb6163cb23951853f88984b217a11aee8ed1
Fun Family <hungpx2008@gmail.com>
2025-09-08 15:09:27 +0000

    thay đổi màu cảu thêm thành mày grdient của lightblue  (_for element mai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { DepartmentSelector } from "@/components/department-selector";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+    <main className="flex min-h-screen flex-col items-center justify-center p-8 bg-gradient-to-br from-blue-100 via-blue-200 to-cyan-200">
       <DepartmentSelector />
     </main>
   );
