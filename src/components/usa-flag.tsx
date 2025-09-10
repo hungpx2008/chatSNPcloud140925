96fd9b6a2c57c1833ae6c7e6d398624c2a3b9046
Fun Family <hungpx2008@gmail.com>
2025-09-10 09:28:35 +0000

    icon trong thay đổi ngôn ngữ hãy lấy file này để đưa vào thay vè bạn vẽ

--- src/components/usa-flag.tsx
+++ src/components/usa-flag.tsx
@@ -2,33 +2,51 @@ export function UsaFlagIcon({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 36 36"
+      viewBox="0 0 1235 650"
       className={className}
-      width="24"
-      height="24"
+      width="36"
+      height="19"
     >
+      <rect width="1235" height="650" fill="#FFF" />
       <path
+        d="M0 0h1235v50H0zM0 100h1235v50H0zM0 200h1235v50H0zM0 300h1235v50H0zM0 400h1235v50H0zM0 500h1235v50H0zM0 600h1235v50H0z"
         fill="#B22234"
-        d="M32 5H4a4 4 0 0 0-4 4v18a4 4 0 0 0 4 4h28a4 4 0 0 0 4-4V9a4 4 0 0 0-4-4z"
       />
-      <path
-        fill="#fff"
-        d="M36 11h-2V9a4 4 0 0 0-4-4h-2v6h-6V5h-2v6h-6V5h-2v6H4a4 4 0 0 0-4 4v2h10v-2h6v2h6v-2h6v2h6v-2a4 4 0 0 0 0-2zM0 25h10v-2H0v2zm14 0h6v-2h-6v2zm10 0h6v-2h-6v2zm-4 6H4a4 4 0 0 0-1 .12V29h33v2.12a4 4 0 0 0-1 0H4a4 4 0 0 0-4 4v2h36v-6h-6z"
-      />
-      <path
-        fill="#3C3B6E"
-        d="M16 5H4a4 4 0 0 0-4 4v8h20V5h-4z"
-      />
-      <circle fill="#fff" cx="3" cy="8" r="1" />
-      <circle fill="#fff" cx="7" cy="8" r="1" />
-      <circle fill="#fff" cx="11" cy="8" r="1" />
-      <circle fill="#fff" cx="5" cy="10" r="1" />
-      <circle fill="#fff" cx="9" cy="10" r="1" />
-      <circle fill="#fff" cx="3" cy="12" r="1" />
-      <circle fill="#fff" cx="7" cy="12" r="1" />
-      <circle fill="#fff" cx="11" cy="12" r="1" />
-      <circle fill="#fff" cx="5" cy="14" r="1" />
-      <circle fill="#fff" cx="9" cy="14" r="1" />
+      <rect width="494" height="350" fill="#3C3B6E" />
+      <g fill="#FFF">
+        <g id="s5">
+          <g id="s4">
+            <path id="s" d="M117.5 0l25 80-65-50h80l-65 50z" />
+            <use href="#s" x="82.333" />
+          </g>
+          <use href="#s4" x="164.666" />
+        </g>
+        <g id="r6">
+          <use href="#s5" y="29.167" />
+          <use href="#s" x="41.167" y="29.167" />
+        </g>
+        <g id="r5">
+          <use href="#s5" x="41.167" y="58.333" />
+        </g>
+        <g id="r4">
+          <use href="#r6" y="58.333" />
+        </g>
+        <g id="r3">
+          <use href="#r5" y="58.333" />
+        </g>
+        <g id="r2">
+          <use href="#r4" y="58.333" />
+        </g>
+        <use href="#r6" transform="translate(41.167 29.167)" />
+        <use href="#r5" transform="translate(0 58.333)" />
+        <use href="#r6" transform="translate(41.167 87.5)" />
+        <use href="#r5" transform="translate(0 116.666)" />
+        <use href="#r6" transform="translate(41.167 145.833)" />
+        <use href="#r5" transform="translate(0 175)" />
+        <use href="#r6" transform="translate(41.167 204.167)" />
+        <use href="#r5" transform="translate(0 233.333)" />
+        <use href="#r6" transform="translate(41.167 262.5)" />
+      </g>
     </svg>
   );
 }
